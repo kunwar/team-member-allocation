615c22fa90683670498450b8bc935e10eb08abb4
Kunwar Singh <dev@kunwar.me>
2023-02-11 19:56:49 +0530

    Add Header.js component to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <main>Hello World!</main>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
