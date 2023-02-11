98f3dcd310489aa3bd2c5e54261ec1431a32e729
Kunwar Singh <dev@kunwar.me>
2023-02-11 22:19:48 +0530

    Render employee data on the main page

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import femaleProfile from '../images/femaleProfile.jpg';
+import maleProfile from '../images/maleProfile.jpg';
 
 const Employees = () => {
   const [employees, setEmployees] = useState([
@@ -89,10 +91,50 @@ const Employees = () => {
   ]);
 
   return (
-    <main>
-      {employees.map(employee => (
-        <p>{employee.fullName}</p>
-      ))}
+    <main className='container'>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-6'>
+          <select className='form-select form-select-lg'>
+            <option value='TeamA'>TeamA</option>
+            <option value='TeamB'>TeamB</option>
+            <option value='TeamC'>TeamC</option>
+            <option value='TeamD'>TeamD</option>
+          </select>
+        </div>
+      </div>
+      <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-8'>
+          <div className='card-collection'>
+            {employees.map(employee => {
+              return (
+                <div
+                  id={employee.id}
+                  className='card m-2'
+                  style={{ cursor: 'pointer' }}
+                >
+                  <img
+                    src={
+                      employee.gender === 'male' ? maleProfile : femaleProfile
+                    }
+                    className='card-img-top'
+                    alt={employee.fullName}
+                  />
+
+                  <div className='card-body'>
+                    <h5 className='card-title'>
+                      Full Name: {employee.fullName}
+                    </h5>
+                    <p className='card-text'>
+                      <b>Designation: </b>
+                      {employee.designation}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
